96161b1fcb961a03bf843ed975a8cbf1a2a56132
Gaurav Bajaj <goru97@gmail.com>
2016-03-23 15:52:28 -0700

    Adding foodcitric fixes

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,12 @@ long_description 'Installs/Configures ele-cassandra'
 version          '0.1.0'
 depends          'cassandra-dse'
 depends          'python'
+depends          'apt'
+depends          'java'
+
+if respond_to?(:source_url)
+  source_url 'https://github.com/mmi-cookbooks/ele-cassandra'
+end
+if respond_to?(:issues_url)
+  issues_url 'https://github.com/mmi-cookbooks/ele-cassandra/issues'
+end
